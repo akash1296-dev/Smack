541aef0036f51c9ed868e610133f9333d7135fcf
Aakash <akash1296@gmail.com>
2019-09-19 17:53:03 +0530

    lesson-04

--- app/src/main/java/com/example/smack/LoginActivity.kt
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,7 +17,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
 }
